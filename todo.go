b03ed4046a36a762adf4cb5e3d0ae48eac5767a8
Junior <eecfredes@gmail.com>
2022-08-21 19:58:10 -0300

    Feat: Todo Function

--- todo.go
+++ todo.go
@@ -1,10 +1,39 @@
 package todo
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type item struct {
 	Task        string
 	Done        bool
 	CreatedAt   time.Time
 	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Add(task string) {
+
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("Invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
 }
