8e3c3bddceb26c71adef1aeec2ac70bd12f8a73d
Junior <eecfredes@gmail.com>
2022-08-22 20:52:32 -0300

    :sparkles: Feat: Todo - Delete

--- todo.go
+++ todo.go
@@ -37,3 +37,14 @@ func (t *Todos) Complete(index int) error {
 
 	return nil
 }
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("Invalid index")
+	}
+
+	*t = append(ls[:index-1], ls[index:]...)
+
+	return nil
+}
