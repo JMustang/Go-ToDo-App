c7da1336330ffa203fb1b9721bd523751abf72f0
Junior <eecfredes@gmail.com>
2022-08-23 20:17:01 -0300

    :sparkles: Feat: Todo - Load

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -45,6 +48,26 @@ func (t *Todos) Delete(index int) error {
 	}
 
 	*t = append(ls[:index-1], ls[index:]...)
+
+	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
