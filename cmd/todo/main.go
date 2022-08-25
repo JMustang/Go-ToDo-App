6f57d3db28e3279b9d8327e8b3e9a22f138ab8a2
Junior <eecfredes@gmail.com>
2022-08-24 21:47:21 -0300

    :sparkles: Feat: Todo | Flag - Add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/JMustang/Go-ToDo-App"
+)
+
+const (
+	todofile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todofile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todofile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "Invalid command!")
+		os.Exit(0)
+	}
 }
